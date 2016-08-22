30223adbe8cc72e101dba72792e92550df170013
cacody <cacody@umich.edu>
2016-08-22 10:17:21 -0400

    series of hot fixes related mostly to styles - part of post launch cleaning

--- template.php
+++ template.php
@@ -8,7 +8,11 @@ function bravil_preprocess_html(&$variables) {
   drupal_add_js('https://use.fontawesome.com/971844ebf1.js', array('type' => 'external'));
 }
 
+function bravil_menu_tree__menu_research_day($variables){
+    return "<ul class=\"nav nav-pills\">\n" . $variables['tree'] . "</ul>\n";
+}
 
-
-
+function bravil_menu_tree__menu_ohs_phd($variables){
+    return "<ul class=\"nav nav-pills\">\n" . $variables['tree'] . "</ul>\n";
+}
 
